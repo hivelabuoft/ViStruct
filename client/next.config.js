645b5151beedfb90d2d9e166241495393f6debb6
Oliver Huang <oliver.h0803@gmail.com>
2025-04-11 21:23:42 -0400

    updated webpack

--- client/next.config.js
+++ client/next.config.js
@@ -1,30 +1,8 @@
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-
+// client/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-
-    config.plugins.push(
-      new MonacoWebpackPlugin({
-        languages: ["json"],
-      })
-    );
-
-    return config;
   },
 };
 
